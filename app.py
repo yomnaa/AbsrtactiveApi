0d7bf6907d5e3f6548d848d31ea064f053b43346
Yomna <yomnaismail95@gmail.com>
2018-06-21 22:38:38 +0200

    adding app

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+import os
+import sys
+import logging
+from flask_cors import CORS
+from serve import getModelApi  # see part 1.
+from Parser import parse
+app = Flask(__name__)
+CORS(app) # needed for cross-domain requests, allow everything by default
+preprocessor_api,model_api = getModelApi()
+
+
+# logging for heroku
+if 'DYNO' in os.environ:
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.INFO)
+
+# API route
+@app.route('/api', methods=['POST'])
+def api():
+    """API function
+
+    All model-specific logic to be defined in the get_model_api()
+    function
+    """
+    summaries=[]
+    articles=[]
+    input_data = request.json
+    i=0
+    for url in input_data:
+        app.logger.info("api_input: " + str(url))
+        p=preprocessor_api(parse(url))
+        # open('art'+str(i),'w').write(p)
+        articles.append(p)
+        app.logger.info("article text: " + articles[-1])
+        i+=1
+
+    summaries= model_api(articles)
+    # i=0
+    app.logger.info("summariescount: " + str(len(summaries))+" "+str(len(articles)))
+    # for summary in summaries:
+    #     open('sum'+str(i)+'.txt','w').write(summary)
+    #     i+=1
+    # app.logger.info("summarized article text: " + output_data)
+
+    # response = jsonify(summaries)
+    response = jsonify(summaries)
+    return response
+
+# default route
+@app.route('/')
+def index():
+    return "Index API"
+
+# HTTP Errors handlers
+@app.errorhandler(404)
+def url_error(e):
+    return """
+    Wrong URL!
+    <pre>{}</pre>""".format(e), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
